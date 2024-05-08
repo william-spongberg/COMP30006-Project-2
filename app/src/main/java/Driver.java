ffabdf83dab42e145a99c2fa2c3744e6b6884dd9
William Spongberg <william.spongberg@outlook.com>
2024-05-08 17:39:27 +1000

    Added Driver notes

--- app/src/main/java/Driver.java
+++ app/src/main/java/Driver.java
@@ -1,11 +1,13 @@
 import java.util.Properties;
 
 public class Driver {
-    public static final String DEFAULT_PROPERTIES_PATH = "properties/game1.properties";
+    public static final String DEFAULT_PROPERTIES_PATH = "properties/game2.properties";
 
     public static void main(String[] args) {
         final Properties properties = PropertiesLoader.loadPropertiesFile(DEFAULT_PROPERTIES_PATH);
+        // TODO: check if properties is null, if so, print error message and return
         String logResult = new LuckyThirdteen(properties).runApp();
+        // hate this, should create game, then run, then get result
         System.out.println("logResult = " + logResult);
     }
 
