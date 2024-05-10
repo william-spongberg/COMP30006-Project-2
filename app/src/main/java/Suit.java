6cf4740e7bb21cb59f7e023ece33836bc87e2b97
jlinehan-01 <jlinehan@student.unimelb.edu.au>
2024-05-10 13:46:47 +1000

    move enums to separate files

---
+++ app/src/main/java/Suit.java
@@ -0,0 +1,21 @@
+public enum Suit {
+    SPADES("S", 4), HEARTS("H", 3),
+    DIAMONDS("D", 2), CLUBS("C", 1);
+
+    private String suitShortHand = "";
+    private int multiplicationFactor = 1;
+    public static final int PUBLIC_CARD_MULTIPLICATION_FACTOR = 2;
+
+    Suit(String shortHand, int multiplicationFactor) {
+        this.suitShortHand = shortHand;
+        this.multiplicationFactor = multiplicationFactor;
+    }
+
+    public String getSuitShortHand() {
+        return suitShortHand;
+    }
+
+    public int getMultiplicationFactor() {
+        return multiplicationFactor;
+    }
+}
