0edbaf6c9c9993df885d21f3f16d7e379fa43f0b
William Spongberg <william.spongberg@outlook.com>
2024-05-15 14:18:04 +1000

    Fixed PlayerFactory not considering initialCards of each Player

--- app/src/main/java/_player/PlayerFactory.java
+++ app/src/main/java/_player/PlayerFactory.java
@@ -13,16 +13,16 @@ public class PlayerFactory {
     // factory for player + playercontroller + hand
     // return? can return player? or only player info?
 
-    public Player createPlayer(String playerType, String initialCards, boolean isAuto, List<String> autoMovements) {
+    public Player createPlayer(String playerType, String initialCards, String initialSharedCards, boolean isAuto, List<String> autoMovements) {
 
         if (playerType.equals("human")) {
-            return new Human(initialCards, isAuto, autoMovements);
+            return new Human(initialCards, initialSharedCards, isAuto, autoMovements);
         } else if (playerType.equals("random")) {
-            return new RandomBot(initialCards, isAuto, autoMovements);
+            return new RandomBot(initialCards, initialSharedCards, isAuto, autoMovements);
         } else if (playerType.equals("basic")) {
-            return new BasicBot(initialCards, isAuto, autoMovements);
+            return new BasicBot(initialCards, initialSharedCards, isAuto, autoMovements);
         } else if (playerType.equals("clever")) {
-            return new CleverBot(initialCards, isAuto, autoMovements);
+            return new CleverBot(initialCards, initialSharedCards, isAuto, autoMovements);
         } else {
             System.err.println("Invalid player type: " + playerType);
             return null;
