7b39e98ffcd9d11066520dbb8ed1c31e1fa87ae8
William Spongberg <william.spongberg@outlook.com>
2024-05-15 14:17:03 +1000

    Added CardListener to ManualController

--- app/src/main/java/_player/controller/ManualController.java
+++ app/src/main/java/_player/controller/ManualController.java
@@ -1,10 +1,22 @@
 package _player.controller;
 
 import ch.aplu.jcardgame.Card;
+import ch.aplu.jcardgame.CardAdapter;
+import ch.aplu.jcardgame.CardListener;
 import ch.aplu.jcardgame.Hand;
 import ch.aplu.jgamegrid.GameGrid;
 
 public class ManualController implements PlayerController {
+
+    Card selected = null;
+    CardListener cardListener = new CardAdapter() // Human Player plays card
+    {
+        @Override
+        public void leftDoubleClicked(Card card) {
+            this.selected = card;
+            //hands[0].setTouchEnabled(false);
+        }
+    };
     
     public Card getMove(Hand hand) {
         hand.setTouchEnabled(true);
@@ -12,9 +24,9 @@ public class ManualController implements PlayerController {
         // set the status message and deal a card to player 0
         //setStatus("Player 0 is playing. Please double click on a card to discard");
         System.out.println("Player 0 is playing. Please double click on a card to discard");
-        
-        Card selected = null;
+
         //dealACardToHand(hand); // TODO: remove, should be done already by dealer
+        selected = null;
 
         // wait until a card is selected
         while (selected == null) {
@@ -24,4 +36,6 @@ public class ManualController implements PlayerController {
         // remove selected card from the hand
         selected.removeFromHand(true);
     }
+
+    
 }
