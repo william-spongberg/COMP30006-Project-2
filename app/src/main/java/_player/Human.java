6e0dab77010f4b162f7179138a825b1a3102387c
William Spongberg <william.spongberg@outlook.com>
2024-05-15 13:26:16 +1000

    Added basic Player + Human code to outline use of controllers + initialisation

--- app/src/main/java/_player/Human.java
+++ app/src/main/java/_player/Human.java
@@ -1,5 +1,24 @@
 package _player;
 
-public class Human implements Player {
+import _player.controller.PlayerController;
+
+import ch.aplu.jcardgame.Card;
+import ch.aplu.jcardgame.Hand;
+
+import java.util.List;
+
+public class Human extends Player {
+    
+    public Human(PlayerController controller, Hand hand) {
+        super(controller, hand);
+    }
+
+    public Human(String initialCards, boolean isAuto, List<String> autoMovements) {
+        super(isAuto, initialCards, autoMovements);
+    }
+
+    public Card getMove() { // give cards on table as param?
+        return getController().getMove(getHand());
+    }
     
 }
