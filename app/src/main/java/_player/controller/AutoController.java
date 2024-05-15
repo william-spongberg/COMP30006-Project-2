d0e732ad43888d2e2064b1b6a63a567a20c57f73
William Spongberg <william.spongberg@outlook.com>
2024-05-15 13:22:31 +1000

    Added initial controller class setups

--- app/src/main/java/_player/controller/AutoController.java
+++ app/src/main/java/_player/controller/AutoController.java
@@ -2,65 +2,65 @@ package _player.controller;
 
 import ch.aplu.jcardgame.Card;
 import ch.aplu.jcardgame.Hand;
+import ch.aplu.jgamegrid.GameGrid;
 
-public class AutoController implements PlayerController{
-    
-    private Card applyAutoMovement(Hand hand, String nextMovement) {
-        if (pack.isEmpty())
-            return null;
-        String[] cardStrings = nextMovement.split("-");
-        String cardDealtString = cardStrings[0];
-        Card dealt = getCardFromList(pack.getCardList(), cardDealtString);
-        if (dealt != null) {
-            dealt.removeFromHand(false);
-            hand.insert(dealt, true);
-        } else {
-            System.out.println("cannot draw card: " + cardDealtString + " - hand: " + hand);
-        }
+import java.util.Properties;
 
-        if (cardStrings.length > 1) {
-            String cardDiscardString = cardStrings[1];
-            return getCardFromList(hand.getCardList(), cardDiscardString);
-        } else {
-            return null;
-        }
+import _card.CardUtil;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class AutoController implements PlayerController {
+
+    private List<String> autoMovements = new ArrayList<>();
+
+    public AutoController(List<String> autoMovements) {
+        this.autoMovements = autoMovements;
     }
 
-    private void setupPlayerAutoMovements() {
-        String player0AutoMovement = properties.getProperty("players.0.cardsPlayed");
-        String player1AutoMovement = properties.getProperty("players.1.cardsPlayed");
-        String player2AutoMovement = properties.getProperty("players.2.cardsPlayed");
-        String player3AutoMovement = properties.getProperty("players.3.cardsPlayed");
+    public Card getMove(Hand hand) {
+        // TODO: convert to Card?
+        //return autoMovements.remove(0);
+        Card card = autoMovements.remove(0);
 
-        // FIXME: playerMovements should be immediately initialised with properties,
-        // remove if statements
-        String[] playerMovements = new String[] { "", "", "", "" };
+        // pretend to think
+        GameGrid.delay(delayTime);
 
-        if (player0AutoMovement != null) {
-            playerMovements[0] = player0AutoMovement;
+        // if auto move available, return card
+        if (card != null) {
+            card.removeFromHand(false);
         }
-
-        if (player1AutoMovement != null) {
-            playerMovements[1] = player1AutoMovement;
+        else {
+            // if no auto move available, get random card and remove from hand
+            card = getRandomCard(hand);
+            card.removeFromHand(true);
         }
-
-        if (player2AutoMovement != null) {
-            playerMovements[2] = player2AutoMovement;
-        }
-
-        if (player3AutoMovement != null) {
-            playerMovements[3] = player3AutoMovement;
-        }
-
-        for (int i = 0; i < playerMovements.length; i++) {
-            // FIXME: unnecessary to define here
-            String movementString = playerMovements[i];
-            if (movementString.equals("")) {
-                playerAutoMovements.add(new ArrayList<>());
-                continue;
-            }
-            List<String> movements = Arrays.asList(movementString.split(","));
-            playerAutoMovements.add(movements);
-        }
+        
+        return card;
     }
+
+    // public Card applyAutoMovement(Hand hand, String nextMovement) {
+    //     // if (pack.isEmpty())
+    //     //     return null;
+
+    //     String[] cardStrings = nextMovement.split("-");
+    //     String cardDealtString = cardStrings[0];
+    //     Card dealt = CardUtil.getCardFromList(pack.getCardList(), cardDealtString);
+
+    //     if (dealt != null) {
+    //         dealt.removeFromHand(false);
+    //         hand.insert(dealt, true);
+    //     } else {
+    //         System.out.println("cannot draw card: " + cardDealtString + " - hand: " + hand);
+    //     }
+
+    //     if (cardStrings.length > 1) {
+    //         String cardDiscardString = cardStrings[1];
+    //         return CardUtil.getCardFromList(hand.getCardList(), cardDiscardString);
+    //     } else {
+    //         return null;
+    //     }
+    // }
 }
--- app/src/main/java/_player/controller/ManualController.java
+++ app/src/main/java/_player/controller/ManualController.java
@@ -1,5 +1,27 @@
 package _player.controller;
 
-public class ManualController implements PlayerController{
+import ch.aplu.jcardgame.Card;
+import ch.aplu.jcardgame.Hand;
+import ch.aplu.jgamegrid.GameGrid;
+
+public class ManualController implements PlayerController {
     
+    public Card getMove(Hand hand) {
+        hand.setTouchEnabled(true);
+
+        // set the status message and deal a card to player 0
+        //setStatus("Player 0 is playing. Please double click on a card to discard");
+        System.out.println("Player 0 is playing. Please double click on a card to discard");
+        
+        Card selected = null;
+        //dealACardToHand(hand); // TODO: remove, should be done already by dealer
+
+        // wait until a card is selected
+        while (selected == null) {
+            GameGrid.delay(delayTime);
+        }
+
+        // remove selected card from the hand
+        selected.removeFromHand(true);
+    }
 }
