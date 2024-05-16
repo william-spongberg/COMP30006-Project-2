87dd7a4a90b228e89cbe0fa03543cb42ccc5b214
William Spongberg <william.spongberg@outlook.com>
2024-05-16 15:07:20 +1000

    Cleaned up AutoController code

--- app/src/main/java/game/_player/controller/AutoController.java
+++ app/src/main/java/game/_player/controller/AutoController.java
@@ -20,7 +20,6 @@ public class AutoController implements PlayerController {
     }
 
     public Card drawCard() {
-        //GameGrid.delay(delayTime);
         if (getAutoMovements().get(autoIndex).size() == 2) {
             System.out.println("auto moves: " + getAutoMovements());
             return getAutoMovements().get(autoIndex).remove(0);
@@ -33,24 +32,15 @@ public class AutoController implements PlayerController {
     }
 
     public Card discardCard(Hand hand) {
-        //GameGrid.delay(delayTime);
-
         // if list within list is size 2, then it is card wanting to discard
         // else if list within list is size 1, then it is the card wanting to draw
 
         System.out.println("auto moves: " + getAutoMovements());
-
-        //GameGrid.delay(delayTime);
         if (getAutoMovements().get(autoIndex).size() == 1) {
             autoIndex++;
-            System.out.println("auto moves: " + getAutoMovements());
-            return getAutoMovements().get(autoIndex-1).remove(0);
+            return getAutoMovements().get(autoIndex - 1).remove(0);
         } else {
-            // DEBUG: print size
-            System.out.println("auto movements size: " + getAutoMovements().get(autoIndex).size());
-            
             // pick random card to discard otherwise
-            System.out.println("auto moves: " + getAutoMovements());
             return hand.get(random.nextInt(hand.getNumberOfCards()));
         }
     }
@@ -62,27 +52,4 @@ public class AutoController implements PlayerController {
     public void setAutoMovements(List<List<Card>> autoMovements) {
         this.autoMovements = autoMovements;
     }
-
-    // public Card applyAutoMovement(Hand hand, String nextMovement) {
-    //     // if (pack.isEmpty())
-    //     //     return null;
-
-    //     String[] cardStrings = nextMovement.split("-");
-    //     String cardDealtString = cardStrings[0];
-    //     Card dealt = CardUtil.getCardFromList(pack.getCardList(), cardDealtString);
-
-    //     if (dealt != null) {
-    //         dealt.removeFromHand(false);
-    //         hand.insert(dealt, true);
-    //     } else {
-    //         System.out.println("cannot draw card: " + cardDealtString + " - hand: " + hand);
-    //     }
-
-    //     if (cardStrings.length > 1) {
-    //         String cardDiscardString = cardStrings[1];
-    //         return CardUtil.getCardFromList(hand.getCardList(), cardDiscardString);
-    //     } else {
-    //         return null;
-    //     }
-    // }
 }
