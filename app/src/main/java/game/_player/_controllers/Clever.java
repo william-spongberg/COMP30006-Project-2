40b505afd25ce27def879e32af25593efe06e323
Ethan Hawkins <ewhawkins@student.unimelb.edu.au>
2024-05-18 12:42:12 +1000

    implementing monte carlo structure

--- app/src/main/java/game/_player/_controllers/Clever.java
+++ app/src/main/java/game/_player/_controllers/Clever.java
@@ -35,9 +35,45 @@ public class Clever implements PlayerController {
         // we know what card we have and what are private
         // the goal: estimate which card we should remove using monte carlo
 
+    // THE PROCESS:
+        // using the cardsPlayed, which contains all discarded cards, and the amount of cards left, simulate all possible
+        // hands and moves of other players through Monte Carlo estimation
+        // decide what card we should discard of the up to three in our hand (always at least two) based on probability of getting 13
+        // we can get thirteen from 3 different ways:
+            // two private cards in hand add to thirteen
+            // one private card in hand and one in from two public card
+            // two public cards and two private cards
+        // only four rounds
     // return index of card to discard
     // apply monte carlo estimation
     private Integer monteCarloMoveEstimation(Hand packRemaining, ArrayList<Card> cardsPlayed, Hand hand) {
+        ArrayList<Card> cardsInHand = hand.getCardList();
+
+        // set up simulation params
+        int simulations = 1000;
+
+        // track successes for each time we discard in the sims
+        int[] discardSucceses = new int[cardsInHand.size()];
+
+        // simulate
+        for (int i = 0; i < simulations; i++) {
+            // get what we know is left in the deck
+            ArrayList<Card> remainingDeck = new ArrayList<>(packRemaining.getCardList());
+            remainingDeck.removeAll(cardsPlayed);
+            remainingDeck.removeAll(cardsInHand);
+
+            for (int j = 0; j < cardsInHand.size(); j++) {
+                // simulate for each card in your hand
+
+                // draw cards for other players
+
+                // check if any combo sums to 13
+            }
+        }
+        // find the card with the highest success rate, discard
+
+
+
         return 0;
     }
 }
