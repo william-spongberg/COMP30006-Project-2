14491d3862f1dab8ab48a815ec81dedac444dc0b
William Spongberg <william.spongberg@outlook.com>
2024-05-20 17:38:59 +1000

    Fixed null error in discardCards

--- app/src/main/java/game/DiscardPile.java
+++ app/src/main/java/game/DiscardPile.java
@@ -1,11 +1,13 @@
 package game;
 
 import ch.aplu.jcardgame.Card;
+
+import java.util.ArrayList;
 import java.util.List;
 // info expert for use by clever.
 public class DiscardPile {
 
-    public static List<Card> discardCards;
+    public static List<Card> discardCards = new ArrayList<>();
 
     public static List<Card> getDiscardCards() {
         return discardCards;
